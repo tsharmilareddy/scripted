67593b65043b3ba212a6bac52bbf42e5f95b8d30
tsharmilareddy <tsharmilareddy47@gmail.com>
2022-11-01 06:27:48 +0530

    updated dockerfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage('ci') {
-        aws ecr get-login-password --region us-east-2 | sudo docker login --username AWS --password-stdin 109657631540.dkr.ecr.us-east-2.amazonaws.com
+        aws ecr get-login-password --region us-east-2 |  docker login --username AWS --password-stdin 109657631540.dkr.ecr.us-east-2.amazonaws.com
          sudo docker build -t 109657631540.dkr.ecr.us-east-2.amazonaws.com/ecr:latest
        sudo docker push 109657631540.dkr.ecr.us-east-2.amazonaws.com/ecr:latest
     }
